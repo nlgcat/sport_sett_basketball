99cf0a9c5e1ae1155152678327974e0948fa344d
Ashish Upadhyay <panditu2015@gmail.com>
2021-01-22 04:27:44 +0000

    Update combine.sh

--- combine.sh
+++ combine.sh
@@ -1,6 +1,6 @@
 cd ./exported_files
-cat D1_2015_data.txt D1_2015_data.txt D1_2015_data.txt > D1_training_data.txt
-cat D1_2015_text.txt D1_2015_text.txt D1_2015_text.txt > D1_training_text.txt
+cat D1_2014_data.txt D1_2015_data.txt D1_2016_data.txt > D1_training_data.txt
+cat D1_2014_text.txt D1_2015_text.txt D1_2016_text.txt > D1_training_text.txt
 cp D1_2017_data.txt D1_valid_data.txt
 cp D1_2017_text.txt D1_valid_text.txt
 cp D1_2018_data.txt D1_test_data.txt
