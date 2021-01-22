d8587eec37feaa32b773be5b880f374b22d11125
Ashish Upadhyay <panditu2015@gmail.com>
2021-01-22 03:36:57 +0000

    Create combine.sh
    Not too much, only need to run one argument instead of 6 for combining the seasons :)

---
+++ combine.sh
@@ -0,0 +1,7 @@
+cd ./exported_files
+cat D1_2015_data.txt D1_2015_data.txt D1_2015_data.txt > D1_training_data.txt
+cat D1_2015_text.txt D1_2015_text.txt D1_2015_text.txt > D1_training_text.txt
+cp D1_2017_data.txt D1_valid_data.txt
+cp D1_2017_text.txt D1_valid_text.txt
+cp D1_2018_data.txt D1_test_data.txt
+cp D1_2018_text.txt D1_test_text.txt
